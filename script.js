872e252c028da463e738cc2eb05c9bf0681946b5
EdwinKinloch <social@kinloch.me>
2019-06-30 20:04:59 -0500

    Added a getJSON line of code that will grab a random picture.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+$.getJSON('https://api.unsplash.com/photos/random/?client_id=6caf1758c800234726cbbf3ed443fb1a53b66304f32d7447f72900bb2ed5c51c', function(data){
+    console.log(data);
+
+
+
+});
