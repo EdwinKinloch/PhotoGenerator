692782537bb8faaf5ab9fb200d3f80ad101a3de4
EdwinKinloch <social@kinloch.me>
2019-07-01 15:46:26 -0500

    Created two vars to capture the photo's description and it's url. Using the vars I used the append method to add the url and description to the two spans.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 $.getJSON('https://api.unsplash.com/photos/random/?client_id=6caf1758c800234726cbbf3ed443fb1a53b66304f32d7447f72900bb2ed5c51c', function(data){
     console.log(data);
-
-
+    var photoDescription = data.alt_description;
+    var imageURL = data.urls.regular;
+   
+    $('#picture').append('<img src=' + imageURL + '>');
+    $('#pictureCaption').append('<p>' + photoDescription + '</p>');
 
 });
