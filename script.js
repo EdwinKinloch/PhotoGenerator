c4d03648514c0d673be0240fa0f8dcba794ac5b0
EdwinKinloch <social@kinloch.me>
2019-07-13 00:53:57 -0500

    Changed the var imageURL to imageURLReg. Created a var called downloadLink to store the download to the loaded photo. On line 9 and 24 the line of code will insert the attr href for the a element at #downloadLink.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $.getJSON('https://api.unsplash.com/photos/random/?client_id=6caf1758c800234726cbbf3ed443fb1a53b66304f32d7447f72900bb2ed5c51c', function(data){
     console.log(data);
     var photoDescription = data.alt_description;
-    var imageURL = data.urls.thumb;
+    var imageURLReg = data.urls.regular;
+    var downloadLink = data.links.download;
    
-    $('#picture').append('<img src=' + imageURL + '>');
+    $('#picture').append('<img src=' + imageURLReg + '>');
     $('#pictureCaption').append('<p>' + photoDescription + '</p>');
+    $('#downloadLink').attr('href', downloadLink);
 });
 
 
@@ -12,12 +14,14 @@ $('#randomPicture').click(function(){
   $.getJSON('https://api.unsplash.com/photos/random/?client_id=6caf1758c800234726cbbf3ed443fb1a53b66304f32d7447f72900bb2ed5c51c', function(data){
     console.log(data);
     var photoDescription = data.alt_description;
-    var imageURL = data.urls.thumb;
+    var imageURLReg = data.urls.regular;
+    var downloadLink = data.links.download;
     
     $('#picture').empty();
     $('#pictureCaption').empty();
-    $('#picture').append('<img src=' + imageURL + '>');
+    $('#picture').append('<img src=' + imageURLReg + '>');
     $('#pictureCaption').append('<p>' + photoDescription + '</p>');
+    $('#downloadLink').attr('href', downloadLink);
     });  
 })
 
